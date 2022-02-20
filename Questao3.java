5b778ecaf79e84761e74c013acc892a1119f7810
ALLANVILASBOAS <alan07vb@gmail.com>
2022-02-20 09:55:06 -0300

    ypdate comentarios Questao3

--- Questao3.java
+++ Questao3.java
@@ -1,11 +1,9 @@
 package desafio;
 import java.util.*;
-/*
- * Função:Duas palavras podem ser consideradas anagramas de si mesmas se as letras de uma palavra podem ser realocadas 
- * para formar a outra palavra. Dada uma string qualquer, desenvolva um algoritmo que encontre o número de pares 
- * de substrings que são anagramas.
- * 
- */
+/* Duas palavras podem ser consideradas anagramas de si mesmas se as letras de uma palavra podem ser realocadas 
+   para formar a outra palavra. Dada uma string qualquer, desenvolva um algoritmo que encontre o número de pares 
+   de substrings que são anagramas.
+*/
 public class Questao3 {
 
 	public static void main(String[] args) {
